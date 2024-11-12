9660b376fa756e0ed26d9b4f23028e8fd238cf31
Sourav Sarkar <75407895+souravcit2013@users.noreply.github.com>
2024-11-12 19:19:52 +0530

    commit changes

---
+++ script.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+
+console.log("Hello Parcel")
+
+const h1 = <h2>Hello parcel...h2...</h2>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(h1)
